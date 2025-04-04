3c73e3a7431e475b63951385ee15b69abad0f405
Jimena Garcia <jimena.garcia@cua.uam.mx>
2025-04-04 13:49:04 -0600

    Update analisis_texto.py

--- analisis_texto.py
+++ analisis_texto.py
@@ -1,24 +1,37 @@
-"""
-Incluye funciones para contar el número de sílabas,
-de palabras y de oraciones contenidas en un texto
-que se recibe como entrada.
-"""
 
+"""
+Considerando el diagrama de estructura anterior, implementa el módulo analisis_texto, el cual debe contener, principalmente, las siguientes funciones:
+contar_oraciones(texto): cuenta las oraciones del texto que recibe como parámetro.
+contar_palabras(texto): cuenta las palabras del texto que recibe como parámetro.
+contar_sílabas(texto): cuenta las sílabas del texto que recibe como parámetro.
+
+"""
 
 def contar_oraciones(texto):
-    """Cuenta y devuelve el número de oraciones en texto."""
-    
-    # Completar
-    
+    oraciones = 0
+    for caracter in texto:
+        if caracter in ".!?:;":
+            oraciones += 1
+    return oraciones if oraciones > 0 else 1
 
 def contar_palabras(texto):
-    """Cuenta y devuelve el número de palabras en texto."""
-    
-    # Completar
-
+    palabras = 0
+    en_palabra = False
+    for caracter in texto:
+        if caracter.isspace() or caracter in ".!?,;:":
+            en_palabra = False
+            palabras += 1
+        elif not en_palabra:
+            palabras += 1
+            en_palabra = True
+        else:
+            print()
+    return palabras
 
 def contar_silabas(texto):
-    """Cuenta y devuelve el número de sílabas en texto."""
-    
-    # Completar
-
+    vocales = "aeiouáéíóúAEIOUÁÉÍÓÚ"
+    silabas = 0
+    for caracter in texto:
+        if caracter in vocales:
+            silabas += 1
+    return silabas
