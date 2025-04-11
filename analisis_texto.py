d5bb417ca59691097ace2d81023ba4f574a04436
Jimena Garcia <jimena.garcia@cua.uam.mx>
2025-04-11 12:39:20 -0600

    Update analisis_texto.py

--- analisis_texto.py
+++ analisis_texto.py
@@ -7,22 +7,22 @@ contar_sílabas(texto): cuenta las sílabas del texto que recibe como parámetro
 
 """
 
-def contar_oraciones(texto):
+def contar_oraciones(texto): #Cuenta las oraciones en un texto basado en signos de puntuacion finales (.!?:;)
     oraciones = 0
     for caracter in texto:
-        if caracter in ".!?:;":
+        if caracter in ".!?:;": #Signos que toma en cuenta como puntuacion finales
             oraciones += 1
-    return oraciones if oraciones > 0 else 1
+    return oraciones if oraciones > 0 else 1 #Al menos un signo de puntuacion 
 
-def contar_palabras(texto):
+def contar_palabras(texto): #Considera palabras como secuencias entre espacios
     palabras = 0
-    for caracter in texto:
-        if caracter.isspace() or caracter in ".!?,;:":
+    for caracter in texto:    
+        if caracter.isspace() or caracter in ".!?,;:": #Si hay un epsacio o contiene esos caracteres los cuenta como palabras
             palabras += 1
     return palabras
 
-def contar_silabas(texto):
-    vocales = "aeiouáéíóúAEIOUÁÉÍÓÚ"
+def contar_silabas(texto): #Aproxima el numero de silabas contando vocales
+    vocales = "aeiouáéíóúAEIOUÁÉÍÓÚ" #vocales minusculas con y sin tilde, mayusculas con y sin tilde
     silabas = 0
     for caracter in texto:
         if caracter in vocales:
